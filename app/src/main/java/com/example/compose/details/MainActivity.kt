4762d53539803afa64ed6b7af8110973b4db5a8d
Sungyong An <soupyong@gmail.com>
2024-12-26 12:38:55 +0900

    Jetpack Compose: Preview Internals (#3)
    https://fornewid.medium.com/jetpack-compose-preview-internals-c13d585af4f3

--- app/src/main/java/com/example/compose/details/MainActivity.kt
+++ app/src/main/java/com/example/compose/details/MainActivity.kt
@@ -3,14 +3,15 @@ package com.example.compose.details
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.compose.details.text.style.TextStyleDetailsScreen
+import com.example.compose.details.preview.PreviewInternalsScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 //            DpAsTextUnitScreen()
-            TextStyleDetailsScreen()
+//            TextStyleDetailsScreen()
+            PreviewInternalsScreen()
         }
     }
 }
